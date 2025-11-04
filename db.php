4d9ef71f032768528651b0a8324bb07405b4df72
LiftyPavita <liftypavita@gmail.com>
2025-11-04 15:56:14 +0700

    Perbarui konfigurasi database ke ksi2025

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // db.php - koneksi sederhana menggunakan MySQLi
 $DB_HOST = "localhost";
-$DB_USER = "db_user";
-$DB_PASS = "db_pass";
-$DB_NAME = "nama_database";
+$DB_USER = "root";        // ubah sesuai user MySQL kamu
+$DB_PASS = "";            // ubah jika pakai password
+$DB_NAME = "ksi2025";     // ✅ nama database sudah diperbarui
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
@@ -11,3 +11,4 @@ if ($mysqli->connect_errno) {
     die("Gagal koneksi ke MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 }
 $mysqli->set_charset("utf8mb4");
+?>
